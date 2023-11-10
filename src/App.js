b597ee0eb92215c61a25ac9ae8dcbaa79e318d1d
Sreeram <sreerammmukku@gmail.com>
2023-11-10 15:20:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AgeCalculator from './Components/ageCalc';
+
+function App() {
+  return (
+    <div className="App">
+        <AgeCalculator />
+    </div>  
+  );
+}
+
+export default App;
---
+++ src/Components/ageCalc.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const AgeCalculator = () => {
+  const [dob, setDob] = useState('');
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    const dateParts = dob.split('-'); 
+    
+    if (dateParts.length === 3) {
+      const day = parseInt(dateParts[2], 10);
+      const month = parseInt(dateParts[1], 10);
+      const year = parseInt(dateParts[0], 10);
+
+      if (!isNaN(day) && !isNaN(month) && !isNaN(year)) {
+        const birthDate = new Date(year, month - 1, day); 
+        const currentDate = new Date();
+        const ageInMilliseconds = currentDate - birthDate;
+        const ageInYears = Math.floor(ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000));
+        setAge(ageInYears);
+        return;
+      }
+    }
+
+    alert('Please enter a valid date of birth (dd-mm-yyyy).');
+  };
+
+  return (
+    <div className="age-calculator">
+      <h1>Age Calculator</h1>
+      <h2>Enter Your Date of Birth</h2>
+      <input
+        type="date"
+        value={dob}
+        onChange={(e) => setDob(e.target.value)}
+      />
+      <button onClick={calculateAge} style={{ backgroundColor: 'blue', color: 'white' }}>
+        Calculate Age
+      </button>
+      {age !== null && age !== 0 && <p>You are {age} years old.</p>}
+    </div>
+  );
+};
+
+export default AgeCalculator;
